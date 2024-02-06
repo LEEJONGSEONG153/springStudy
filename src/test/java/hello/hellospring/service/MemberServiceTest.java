e11a11457b8a978a5c0c7156f8e8b94324906285
LEEJONGSEONG153 <96733256+LEEJONGSEONG153@users.noreply.github.com>
2024-02-06 21:33:39 +0900

    jdbc

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -15,7 +15,7 @@ class MemberServiceTest {
     MemoryMemberRepository memberRepository;
 
     @BeforeEach
-    void beforeEach() { 
+    void beforeEach() {
         memberRepository = new MemoryMemberRepository();
         memberService = new MemberService(memberRepository);
     }
